5e30c7b8239974295d4d3162b947f76c871efaf4
jaynaroro <jaynaroro@JaysWin32>
2023-04-07 18:32:04 +0300

    Project Update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Tenzies from '../assets/tenzis.png'
-import Meme from '../assets/MemeGenerator.jpg'
+import Game from '../assets/game.jpg'
 import Vanlife from '../assets/VanLife.jpg'
 import code from '../assets/code.png'
 
@@ -17,7 +17,7 @@ export default function Projects(){
                         view and hire vans posted by van owners.
                     </section>
                     <section className='card-links'>
-                        <a href="https://naroro-vanlife.netlify.app/" target="_blank" rel="noopener noreferrer" >Live Demo</a> <a href='https://github.com/jaynaroro/-VanLife'> <img src={code}/> </a>
+                        <a href="https://naroro-vanlife.netlify.app/" target="_blank" rel="noopener noreferrer" >Live Demo</a> <a href='https://github.com/jaynaroro/-VanLife' target="_blank" rel="noopener noreferrer"> <img src={code}/> </a>
                     </section>
                 </section>
 
@@ -28,18 +28,18 @@ export default function Projects(){
                         The game is simple, roll the die, hold a number, try to match all die with the same number, using the least number of rolls.
                     </section>
                     <section className='card-links'>
-                        <a href="https://naroro-reacttenziesgame.netlify.app/" target="_blank" rel="noopener noreferrer">Live Demo</a> <a href='https://github.com/jaynaroro/react-tenzies-game'> <img src={code}/> </a>
+                        <a href="https://naroro-reacttenziesgame.netlify.app/" target="_blank" rel="noopener noreferrer">Live Demo</a> <a href='https://github.com/jaynaroro/react-tenzies-game' target="_blank" rel="noopener noreferrer"> <img src={code}/> </a>
                     </section>
                 </section>
 
                 <section className='project-card'>
-                    <img className='card-image' src={Meme} />
+                    <img className='card-image' src={Game} />
                     <section className='card-text'>
-                        The Meme Generator is another simple and fun way to demonstrate the usage of State in React. Get a random image throw in some
-                        text to match the image. Have a laugh!
+                        A basic UI React Skeleton for an Ecommerce website. Focussing on the cart logic, the app shows the
+                        interface to be integrated with payment API's such as Stripe or MPESA.
                     </section>
                     <section className='card-links'>
-                        <a href="https://naroro-memegenerator.netlify.app/" target="_blank" rel="noopener noreferrer" >Live Demo</a> <a href='https://github.com/jaynaroro/memegenerator'> <img src={code}/> </a>
+                        <a href="https://naroro-gamingshop.netlify.app/" target="_blank" rel="noopener noreferrer" >Live Demo</a> <a href='https://github.com/jaynaroro/GamingShop' target="_blank" rel="noopener noreferrer"> <img src={code}/> </a>
                     </section>
                 </section>
             </div>
